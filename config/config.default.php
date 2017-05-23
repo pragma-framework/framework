5d492c686e942a9cedccc0b9612325d4cbcebf87
Cyril Rouyer <cyril.rouyer@gmail.com>
2017-05-23 20:59:12 +0200

    EVO - default config & uid
    Default configuration in order to specify how the ORM should handle ids (ai or uids)

--- config/config.default.php
+++ config/config.default.php
@@ -10,3 +10,6 @@ define( 'APP_PATH', realpath('..') . '/app' );
 //AuthSession params
 //define('AUTH_USER_MODEL', 'YOUR MODEL CLASS');
 //define('AUTH_CRYPTO_COST', 10);
+
+define('ORM_ID_AS_UID', false);//set true if you want to have ids as uids instead of autoincrement ids - default strategy : php uniqid('', true)
+define('ORM_UID_STRATEGY', 'php');//define the way you want to create uids (php : uniqid('', true) ; mysql : uuid() - it works only if DB_CONNECTOR is 'mysql')
